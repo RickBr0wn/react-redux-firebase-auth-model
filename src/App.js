7a954b9759036d0a8f51425328f29ebfc2005d8e
Rick Brown <ricky.brown.00@gmail.com>
2019-03-29 21:32:40 +0000

    Routing working

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-import React, { Component } from 'react'
-import ToDoList from './Components/TodoList'
+import React from 'react'
+import { Dashboard, HomePage, Login, Register } from './Components'
+import { DASHBOARD, HOMEPAGE, LOGIN, REGISTER } from './Constants/routes'
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'
+import { CssBaseline } from '@material-ui/core'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <ToDoList />
-      </div>
-    )
+const theme = createMuiTheme({
+  typography: {
+    useNextVariants: true
   }
-}
+})
 
-export default App
+export default function App() {
+  return (
+    <MuiThemeProvider theme={theme}>
+      <CssBaseline />
+      <Router>
+        <Switch>
+          <Route exact path={HOMEPAGE} component={HomePage} />
+          <Route exact path={LOGIN} component={Login} />
+          <Route exact path={REGISTER} component={Register} />
+          <Route exact path={DASHBOARD} component={Dashboard} />
+        </Switch>
+      </Router>
+    </MuiThemeProvider>
+  )
+}
---
+++ src/Components/index.js
@@ -0,0 +1,6 @@
+import Dashboard from './Dashboard'
+import HomePage from './HomePage'
+import Login from './Login'
+import Register from './Register'
+
+export { Dashboard, HomePage, Login, Register }
